f0e76e6388f65afb9849d8e24a5828444c2473a5
2y4n <163217935+2y4n@users.noreply.github.com>
2024-06-04 18:39:32 +0300

    Update erthv2.py

--- erthv2.py
+++ erthv2.py
@@ -62,16 +62,16 @@ def generate_gpt2_response(prompt):
     return response
 
 #generate OpenAI response
-def generate_openai_response(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1000,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    return response.choices[0].text.strip()
+# def generate_openai_response(prompt):
+#     response = openai.Completion.create(
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         max_tokens=1000,
+#         n=1,
+#         stop=None,
+#         temperature=0.7,
+#     )
+#     return response.choices[0].text.strip()
 
 #for the speech-to-speech
 CHUNK_SIZE = 1024
@@ -86,82 +86,82 @@ INPUT_AUDIO_FILE = "input.wav"
 openai.api_key = OPENAI_API_KEY
 
 #record audio
-def record_audio(file_path, record_seconds):
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNK_SIZE)
-    st.write("Recording...")
-    frames = []
-    for _ in range(0, int(44100 / CHUNK_SIZE * record_seconds)):
-        data = stream.read(CHUNK_SIZE)
-        frames.append(data)
-    st.write("Recording complete.")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    with wave.open(file_path, 'wb') as wf:
-        wf.setnchannels(1)
-        wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-        wf.setframerate(44100)
-        wf.writeframes(b''.join(frames))
-    st.write(f"Audio saved to {file_path}")
+# def record_audio(file_path, record_seconds):
+#     p = pyaudio.PyAudio()
+#     stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNK_SIZE)
+#     st.write("Recording...")
+#     frames = []
+#     for _ in range(0, int(44100 / CHUNK_SIZE * record_seconds)):
+#         data = stream.read(CHUNK_SIZE)
+#         frames.append(data)
+#     st.write("Recording complete.")
+#     stream.stop_stream()
+#     stream.close()
+#     p.terminate()
+#     with wave.open(file_path, 'wb') as wf:
+#         wf.setnchannels(1)
+#         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+#         wf.setframerate(44100)
+#         wf.writeframes(b''.join(frames))
+#     st.write(f"Audio saved to {file_path}")
 
 #transcribe audio with whisper
-def transcribe_audio_with_whisper(file_path):
-    st.write(f"Transcribing audio from {file_path}")
-    if not os.path.exists(file_path):
-        st.write(f"File {file_path} does not exist.")
-        return None
-    with open(file_path, 'rb') as audio_file:
-        transcription = openai.Audio.transcribe(model="whisper-1", file=audio_file)
-    return transcription['text']
+# def transcribe_audio_with_whisper(file_path):
+#     st.write(f"Transcribing audio from {file_path}")
+#     if not os.path.exists(file_path):
+#         st.write(f"File {file_path} does not exist.")
+#         return None
+#     with open(file_path, 'rb') as audio_file:
+#         transcription = openai.Audio.transcribe(model="whisper-1", file=audio_file)
+#     return transcription['text']
 
-#generate text AraGPT2
-second_gpt2_model, second_gpt2_tokenizer = load_gpt2_model("rarayayan/testftargpt2")
+# #generate text AraGPT2
+# second_gpt2_model, second_gpt2_tokenizer = load_gpt2_model("rarayayan/testftargpt2")
 
-def generate_text(text, max_length=150, num_return_sequences=1, temperature=0.7, top_p=0.9, top_k=50):
-    text_generator = pipeline("text-generation", model=second_gpt2_model, tokenizer=second_gpt2_tokenizer)
-    generated_text = text_generator(
-        text,
-        max_length=max_length,
-        num_return_sequences=num_return_sequences,
-        temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
-        truncation=True
-    )
-    return generated_text[0]['generated_text']
+# def generate_text(text, max_length=150, num_return_sequences=1, temperature=0.7, top_p=0.9, top_k=50):
+#     text_generator = pipeline("text-generation", model=second_gpt2_model, tokenizer=second_gpt2_tokenizer)
+#     generated_text = text_generator(
+#         text,
+#         max_length=max_length,
+#         num_return_sequences=num_return_sequences,
+#         temperature=temperature,
+#         top_p=top_p,
+#         top_k=top_k,
+#         truncation=True
+#     )
+#     return generated_text[0]['generated_text']
 
 # convert text to speech 
-def convert_text_to_speech(text, output_path):
-    url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
-    querystring = {"output_format":"mp3_22050_32"}
-    payload = {
-        "model_id": "eleven_multilingual_v2",
-        "text": text,
-        "voice_settings": {
-            "stability": 0.15,
-            "similarity_boost": 1,
-            "style": 0.35
-        }
-    }
-    headers = {
-        "xi-api-key": XI_API_KEY,
-        "Content-Type": "application/json"
-    }
-    # response = requests.post(url, json=payload, headers=headers, params=querystring)
-    response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
-    if response.ok:
-        with open(output_path, "wb") as f:
-            for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-                f.write(chunk)
-        st.write("Audio stream saved successfully.")
-    else:
-        st.write(response.text)
+# def convert_text_to_speech(text, output_path):
+#     url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
+#     querystring = {"output_format":"mp3_22050_32"}
+#     payload = {
+#         "model_id": "eleven_multilingual_v2",
+#         "text": text,
+#         "voice_settings": {
+#             "stability": 0.15,
+#             "similarity_boost": 1,
+#             "style": 0.35
+#         }
+#     }
+#     headers = {
+#         "xi-api-key": XI_API_KEY,
+#         "Content-Type": "application/json"
+#     }
+#     # response = requests.post(url, json=payload, headers=headers, params=querystring)
+#     response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+#     if response.ok:
+#         with open(output_path, "wb") as f:
+#             for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+#                 f.write(chunk)
+#         st.write("Audio stream saved successfully.")
+#     else:
+#         st.write(response.text)
 
 
 
 #tabs
-tab1, tab2 = st.tabs(["FT-AraGPT2 Text-to-text", "FT-AraGPT2 Speech-to-speech"])#, "OpenAI API"])
+tab1 = st.tabs(["FT-AraGPT2 Text-to-text"])#, "FT-AraGPT2 Speech-to-speech", "OpenAI API"])
 
 with tab1:
     st.header("Fine-Tuned AraGPT2 Text-To-Text")
@@ -183,34 +183,34 @@ with tab1:
 
         st.session_state.gpt2_messages.append({"role": "assistant", "content": response})
 
-with tab2:
-    st.header("Fine-Tuned AraGPT2 Speech-To-Text")
+# with tab2:
+#     st.header("Fine-Tuned AraGPT2 Speech-To-Text")
 
-    if st.button("Record Audio"):
-        record_audio(INPUT_AUDIO_FILE, RECORD_SECONDS)
+#     if st.button("Record Audio"):
+#         record_audio(INPUT_AUDIO_FILE, RECORD_SECONDS)
 
-    input_audio = os.path.join(".", INPUT_AUDIO_FILE)
-    output_audio = os.path.join(".", OUTPUT_AUDIO_FILE)
+#     input_audio = os.path.join(".", INPUT_AUDIO_FILE)
+#     output_audio = os.path.join(".", OUTPUT_AUDIO_FILE)
 
-    if os.path.exists(input_audio):
-        transcribed_text = transcribe_audio_with_whisper(input_audio)
+#     if os.path.exists(input_audio):
+#         transcribed_text = transcribe_audio_with_whisper(input_audio)
 
-        if transcribed_text:
-            st.write("Transcribed Text:", transcribed_text)
-            generated_text = generate_text(transcribed_text)
-            st.write("Generated Text :", generated_text)
-            convert_text_to_speech(generated_text, output_audio)
-            if os.path.exists(output_audio):
-                st.audio(output_audio)
-                st.write("Playing the generated audio...")
-                audio = AudioSegment.from_file(output_audio)
-                play(audio)
-            else:
-                st.write("Generated audio file does not exist.")
-        else:
-            st.write("Transcription failed.")
-    else:
-        st.write("Please record audio first.")
+#         if transcribed_text:
+#             st.write("Transcribed Text:", transcribed_text)
+#             generated_text = generate_text(transcribed_text)
+#             st.write("Generated Text :", generated_text)
+#             convert_text_to_speech(generated_text, output_audio)
+#             if os.path.exists(output_audio):
+#                 st.audio(output_audio)
+#                 st.write("Playing the generated audio...")
+#                 audio = AudioSegment.from_file(output_audio)
+#                 play(audio)
+#             else:
+#                 st.write("Generated audio file does not exist.")
+#         else:
+#             st.write("Transcription failed.")
+#     else:
+#         st.write("Please record audio first.")
 
 # with tab3:
 #     st.header("OpenAI API")
